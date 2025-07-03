75a9b28bb8e191170346ef7654f962ea6ef913d9
Ioannis Kalogridis <ikalogrides@gmail.com>
2025-07-03 13:06:15 +0300

    First commit of the project

---
+++ MyDiary/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', views.users_register, name="register"),
+    path('login/', views.users_login, name="login"),
+     path('logout/', views.users_logout, name="logout"),
+]
---
+++ MyDiary/users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import logout,login
+# Create your views here.
+def users_register(request):
+    if request.method =="POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request,form.save())
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+    return render(request,'users/register.html',{"form":form})
+
+def users_login(request): 
+    if request.method == "POST": 
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid(): 
+            login(request, form.get_user())
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("posts:list")
+    else: 
+        form = AuthenticationForm()
+    return render(request, "users/login.html", { "form": form })
+
+def users_logout(request):
+    if request.method =="POST":
+        logout(request)
+        return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+    return render(request,'users/login.html',{"form":form})
